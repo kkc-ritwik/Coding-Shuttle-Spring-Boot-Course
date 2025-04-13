a9caf59e87a16b63f9351f0f62feaa5b052c806c
Ritwik Singh <ritwiksingh@192.168.1.8>
2025-04-13 16:06:47 +0530

    Week 1 Spring Boot Codes

---
+++ src/main/java/com/codingshuttle/ritwik/week1Introduction/introductiontospringboot/IntroductiontospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.ritwik.week1Introduction.introductiontospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductiontospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductiontospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshuttle/ritwik/week1Introduction/introductiontospringboot/IntroductiontospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.ritwik.week1Introduction.introductiontospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductiontospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
